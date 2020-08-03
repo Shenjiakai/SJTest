1b00170aa164327b09fdac0f9fdaac7bd513c2e5
沙子 <shenjiakai@shenjiakaideMacBook-Air.local>
2020-08-03 11:51:44 +0800

    初始化

---
+++ testView/test/SJViewController.h
@@ -0,0 +1,13 @@
+//
+//  SJViewController.h
+//  SJFMBase
+//
+//  Created by sjk on 08/03/2020.
+//  Copyright (c) 2020 sjk. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SJViewController : UIViewController
+
+@end
---
+++ testView/test/SJViewController.m
@@ -0,0 +1,29 @@
+//
+//  SJViewController.m
+//  SJFMBase
+//
+//  Created by sjk on 08/03/2020.
+//  Copyright (c) 2020 sjk. All rights reserved.
+//
+
+#import "SJViewController.h"
+
+@interface SJViewController ()
+
+@end
+
+@implementation SJViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
